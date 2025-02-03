391fae097140ee7f171ba47dbe139f3a64a01b76
shahadot99999 <shahadot99999@gmail.com>
2025-02-03 07:51:21 +0600

    map file is added

---
+++ javaScript/map.js
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3, 4];
+const doubled = numbers.map(num => num * 2); // One-line magic!
+//console.log(doubled); // Output: [2, 4, 6, 8]
+
+const numbers1 = [1, 2, 3];
+numbers1.forEach(num => console.log(num)); // Output: 1, 2, 3
